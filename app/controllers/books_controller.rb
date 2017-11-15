f15c3d48acdadbe8e33046c482feee36d60815be
Wojciech Baczyński <baczynskiwojtek@gmail.com>
2017-11-15 21:35:20 +0100

    Api task done.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,12 +3,6 @@ class BooksController < ApplicationController
   before_action :load_book, only: :show
   before_action :new_book, only: :create
 
-  def index
-  end
-
-  def new
-  end
-
   def create
     if new_book.save
       redirect_to books_path
@@ -17,10 +11,34 @@ class BooksController < ApplicationController
     end
   end
 
+  def filter
+    render template: 'books/filter', locals: { books: filter_books }
+  end
+
   def show
   end
 
   private
+
+  def filter_params
+    permitted_params
+        .slice(:title, :isbn)
+        .merge(category.present? ? {category_id: category.id} : {})
+        .reject {|k, v| v.to_s.empty?}
+  end
+
+  def filter_books
+    Book.where(filter_params)
+  end
+
+  def category
+    Category.find_by(name: permitted_params[:category_name])
+  end
+
+  def permitted_params
+    params.permit(:title, :isbn, :category_id, :category_name)
+  end
+
 
   def load_books
     @books = Book.all
